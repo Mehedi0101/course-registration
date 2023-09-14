6107ad68a011c8af1d5aea887f323d9e8d6accb7
Mehedi0101 <mehedi29121998@gmail.com>
2023-09-14 23:43:19 +0600

    cards are added and the contents of fake data are displayed on the card

---
+++ src/components/Course.jsx
@@ -0,0 +1,39 @@
+import Proptypes from 'prop-types';
+import { FiDollarSign } from 'react-icons/fi';
+import { HiOutlineBookOpen } from 'react-icons/hi';
+
+const Course = ({course}) => {
+
+    const { course_title, cover_img, course_description, price, credit } = course;
+
+    return (
+        <div className='p-4 flex flex-col justify-between'>
+            <div>
+                <img className='w-full rounded-lg' src={cover_img} alt="" />
+            </div>
+            <h2 className='text-lg font-semibold text-[#1C1B1B] my-3'>{course_title}</h2>
+            <p className='text-[#1c1b1b99] text-justify text-sm'>{course_description}</p>
+            <div className='my-3'>
+                <div className='flex gap-2 justify-between mb-2 flex-row md:flex-col xl:flex-row'>
+                    <div className='flex items-center gap-1 mx-0 md:mx-auto xl:mx-0'>
+                        <FiDollarSign className='text-xl text-[#1C1B1B]'/>
+                        <p className='text-[#1c1b1b99] font-medium'>Price: {price}</p>
+                    </div>
+                    <div className='flex items-center gap-1 mx-0 md:mx-auto xl:mx-0'>
+                        <HiOutlineBookOpen className='text-2xl text-[#1C1B1B]' />
+                        <p className='text-[#1c1b1b99] font-medium'>Credit: {credit}hrs</p>
+                    </div>
+                </div>
+
+                <button className='bg-blue-500 active:bg-blue-700 text-white text-lg font-semibold w-full py-2 rounded-lg transition-colors duration-75'>Select</button>
+            </div>
+        </div>
+    );
+};
+
+
+Course.propTypes = {
+    course: Proptypes.object.isRequired
+}
+
+export default Course;
