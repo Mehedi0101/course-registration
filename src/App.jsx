bde077b00b9ef1377d4a139f441418a66c2cc8c9
Mehedi0101 <mehedih2909@gmail.com>
2023-09-14 21:09:36 +0600

    fake data added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className='w-11/12 mx-auto'>
+      <h1 className='text-[#1C1B1B] text-2xl md:text-3xl lg:text-4xl font-bold text-center my-5'>Course Registration</h1>
+    </div>
   )
 }
 
