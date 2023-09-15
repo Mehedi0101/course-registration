4065e56b5ae839b18e5a9759573eecf8950e28ba
Mehedi0101 <mehedih2909@gmail.com>
2023-09-15 09:04:44 +0600

    cart contents and functionalities added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import Cart from './components/Cart'
 import Courses from './components/Courses'
 
 function App() {
 
+  const [selectedCourse, setSelectedCourse] = useState([]);
+
+  console.log(selectedCourse);
+  
   return (
     <div className='w-11/12 mx-auto'>
 
       <h1 className='text-[#1C1B1B] text-2xl md:text-3xl lg:text-4xl font-bold text-center my-5'>Course Registration</h1>
 
       <div className='flex gap-6 mt-10 flex-col-reverse md:flex-row'>
-        <Courses></Courses>
-        <Cart></Cart>
+        <Courses selectedCourse={selectedCourse} setSelectedCourse={setSelectedCourse}></Courses>
+        <Cart selectedCourse={selectedCourse}></Cart>
       </div>
 
     </div>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,35 @@
-const Cart = () => {
+import Proptypes from 'prop-types';
+
+const Cart = ({selectedCourse}) => {
+
+    const totalCredit = selectedCourse.reduce((total, selected) => total += selected.credit, 0);
+
+    const totalPrice = selectedCourse.reduce((total, selected) => total += selected.price, 0);
+
     return (
-        <div className="h-96 bg-blue-300 w-full md:w-1/4">
-            Cart
+        <div className="w-full md:w-1/4 p-6">
+            <p className='text-lg font-bold text-blue-500'>Credit Hour Remaining 7 hr</p>
+
+            <hr className='border-[#1c1b1b33] my-4' />
+
+            <h2 className='text-[#1C1B1B] text-xl font-bold'>Course Name</h2>
+            <ol className='list-decimal ml-5 mt-5 leading-8 text-[#1c1b1b99]'>
+                {
+                    selectedCourse.map(selected => <li key={selected.course_id}>{selected.course_title}</li>)
+                }
+            </ol>
+
+            <hr className='mt-6 mb-4 border-[#1c1b1b33]' />
+
+            <p className='text-[#1c1b1bcc] font-medium'>Total Credit Hour : {totalCredit}</p>
+
+            <p className='text-[#1c1b1bcc] font-semibold mt-8'>Total Price : {totalPrice} USD</p>
         </div>
     );
 };
+
+Cart.propTypes = {
+    selectedCourse: Proptypes.object.isRequired
+}
 
 export default Cart;
